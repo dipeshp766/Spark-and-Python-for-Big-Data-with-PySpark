f6a106778df2deb6623f217510b53e716d2a8a0e
dipeshp766 <56689221+dipeshp766@users.noreply.github.com>
2020-07-13 16:58:36 -0400

    Add files via upload

---
+++ SparkStreaming/TweetRead.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import tweepy
+from tweepy import OAuthHandler, Stream
+
+
+# In[ ]:
+
+
+from tweepy.streaming import StreamListener
+import socket
+import json
+
+
+# In[ ]:
+
+
+consumer_key = 'insert your personal consumer key here.'
+consumer_secret = 'insert your personal consumer secret key here.'
+access_token = 'insert your personal access token here.'
+access_secret = 'insert your personal access secret token here.'
+
+
+# In[ ]:
+
+
+class TweetListener(StreamListener):
+    
+    def __init__(self, csocket):
+        self.client_socket = csocket
+        
+    def on_data(self,data):
+        
+        try:
+            msg = json.loads(data)
+            print(msg['text'].encode('utf-8'))
+            self.client_socket.send(msg['text'].encode('utf-8'))
+            return True
+        except BaseException as e:
+            print("ERROR",e)
+        return True
+    
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+# In[ ]:
+
+
+def sendData(c_socket):
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    
+    twitter_stream = Stream(auth, TweetListener(c_socket))
+    twitter_stream.filter(track = ['guitar'])
+
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    s = socket.socket()
+    host = '127.0.0.1'
+    port = 5555
+    s.bind((host,port))
+    
+    print('listening on port 5555')
+    
+    s.listen(5)
+    c,addr = s.accept()
+    
+    sendData(c)
+
+
+# In[ ]:
+
+
+
+
